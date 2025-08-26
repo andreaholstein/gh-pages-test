6fca91d1fbd5b6401338d55378b5057e0125e045
andrea <andrea.gitcommit@gmail.com>
2025-08-25 20:59:32 -0400

    Create Public folder with images

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/gh-pages-test/" // well, whole site blank now on deployment } LOCAL: ../gh-pages-test/ is working for both headshots and cow } setting base as /gh-pages-test/ breaks headshot images but not cow, ../gh-pages-test/ breaks cow but not headshots
+});
